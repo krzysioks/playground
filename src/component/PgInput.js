7028301b5f1239053f4ece866b9667fecb7b65ce
krzysioks <38760633+krzysioks@users.noreply.github.com>
2019-09-24 15:13:02 +0200

    Added validation; submit button disabled if form is not valid

---
+++ src/component/PgInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Input, FormFeedback } from 'reactstrap';
+
+const PgInput = ({ field,
+    form: { touched, errors },
+    ...props }) => (
+        <div>
+            <Input {...field} {...props} invalid={!!(touched[field.name] &&
+                errors[field.name])} />
+            {touched[field.name] &&
+      errors[field.name] && <FormFeedback>{errors[field.name]}</FormFeedback>}
+        </div>
+    );
+
+export default PgInput;
--- src/component/TaskLogin.js
+++ src/component/TaskLogin.js
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { Card, CardBody, CardTitle, Button, Input } from 'reactstrap';
+import PgInput from './PgInput';
+import { Card, CardBody, CardTitle, Button } from 'reactstrap';
 import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+
+const LoginSchema = Yup.object().shape({
+    username: Yup.string()
+        .min(5, 'User name is too short')
+        .max(50, 'User name is too long')
+
+        .required('Username is required'),
+    password: Yup.string()
+        .min(8, 'Password too short')
+        .matches(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()])/, 'Password must have at least one: large and small letter, number, special character "!@#$%^&*())"')
+        .required('Password is required')
+});
 
 const TaskLogin = (props) => {
     // useEffect hook is called after every render. To simulate componentDidMount lifecycle method pass empty array as a second argument. useEffect() will be called after render only if any parameter from the list have changed.
@@ -29,13 +43,15 @@ const TaskLogin = (props) => {
                     <CardTitle>Welcome to Task App</CardTitle>
                     <Formik
                         initialValues={{ username: '', password: '' }}
-                        component={() => (
-                            <Form>
-                                <Field className="mt-1" component={Input} type="text" name="username" placeholder="user name" />
-                                <Field className="mt-1" component={Input} type="password" name="password" placeholder="password" />
-
+                        validationSchema={LoginSchema}//schema used to validate form fields
+                        //below are destructured Formik props, which represent the state of form (i.e. if it is valid or not, if any value changed...)
+                        component={({ isValid, isSubmitting }) => (
+                            < Form >
+                                <Field component={PgInput} name="username" className="mt-1" type="text" placeholder="user name" />
+                                {/* PgInput wraps reactstrap form input component and add error msg placeholder so it is possible to use material design components with Formik */}
+                                <Field component={PgInput} name="password" className="mt-1" type="password" placeholder="password" />
                                 <div className="d-flex flex-row justify-content-between mt-2">
-                                    <Button onClick={_handleOnClick} color="primary">log in</Button>
+                                    <Button disabled={!isValid || isSubmitting} onClick={_handleOnClick} color="primary">log in</Button>
                                     <Link className="align-self-end" to="/task/register">register</Link>
                                 </div>
                             </Form>
@@ -43,7 +59,7 @@ const TaskLogin = (props) => {
                     />
                 </CardBody>
             </Card>
-        </div>
+        </div >
     );
 }
 export default withRouter(TaskLogin);
