104cebe1c969a18b02af10189754e782cc6853f1
krzysioks <38760633+krzysioks@users.noreply.github.com>
2020-01-03 13:08:28 +0100

    prepared production bundles working version

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,28 +1,62 @@
-import webpack from 'webpack';
-import { resolve } from 'path';
+import webpack from "webpack";
+import { resolve } from "path";
 
-import { getIfUtils, removeEmpty } from 'webpack-config-utils';
+import { getIfUtils, removeEmpty } from "webpack-config-utils";
+import UglifyJsPlugin from "uglifyjs-webpack-plugin";
 
 export default env => {
-    const { ifProd, ifNotProd } = getIfUtils(env);
+  const { ifProd, ifNotProd } = getIfUtils(env);
 
-    return {
-        mode: ifProd('production', 'development'),
-        devtool: ifNotProd('cheap-module-source-map'),
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader'
-                    }
-                },
-                {
-                    test: /\.scss$|\.css$/,
-                    loader: 'style-loader!css-loader'
-                }
-            ]
+  return {
+    mode: ifProd("production", "development"),
+    entry: {
+      index: "./src/index.js"
+      //   playground: "./src/Playground.js",
+      //   taskMainView: "./src/component/TaskMainView.js",
+      //   taskLogin: "./src/component/TaskLogin.js",
+      //   taskRegister: "./src/component/TaskRegister.js"
+    },
+    output: {
+      filename: "[name].js",
+      path: resolve(__dirname, "dist")
+    },
+    devtool: ifNotProd("cheap-module-source-map"),
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.scss$|\.css$/,
+          loader: "style-loader!css-loader"
         }
-    };
+      ]
+    },
+    optimization: {
+      splitChunks: {
+        // chunks: "all",
+        // maxSize: 300000,
+        cacheGroups: {
+          //All node_modules into vendors.js
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all"
+          }
+        }
+      },
+      minimizer: removeEmpty([
+        ifProd(
+          new UglifyJsPlugin({
+            cache: true,
+            parallel: true
+          })
+        )
+      ])
+    }
+  };
 };
