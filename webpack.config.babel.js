603af1d9b243a1eab7ac9ab948a8a10cd2d77468
krzysioks <38760633+krzysioks@users.noreply.github.com>
2020-01-07 14:07:36 +0100

    webpack config file updated

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,9 +1,6 @@
 import webpack from "webpack";
 import { resolve } from "path";
-
 import { getIfUtils, removeEmpty } from "webpack-config-utils";
-// import UglifyJsPlugin from "uglifyjs-webpack-plugin";
-// import CompressionPlugin from "compression-webpack-plugin";
 
 export default env => {
   const { ifProd, ifNotProd } = getIfUtils(env);
@@ -12,26 +9,12 @@ export default env => {
     mode: ifProd("production", "development"),
     entry: {
       index: "./src/index.js"
-      //   playground: "./src/Playground.js",
-      //   taskMainView: "./src/component/TaskMainView.js",
-      //   taskLogin: "./src/component/TaskLogin.js",
-      //   taskRegister: "./src/component/TaskRegister.js"
     },
     output: {
       filename: "[name].bundle.js",
       path: resolve(__dirname, "dist")
     },
     devtool: ifNotProd("cheap-module-source-map"),
-    // plugins: [
-    //   ifProd(
-    //     new CompressionPlugin({
-    //       filename: "[path].gz[query]",
-    //       algorithm: "gzip",
-    //       test: /\.(js|json|css)$/,
-    //       deleteOriginalAssets: true
-    //     })
-    //   )
-    // ],
     module: {
       rules: [
         {
@@ -49,8 +32,6 @@ export default env => {
     },
     optimization: {
       splitChunks: {
-        // chunks: "all"
-        // maxSize: 200000
         cacheGroups: {
           //All node_modules into vendors.js
           commons: {
